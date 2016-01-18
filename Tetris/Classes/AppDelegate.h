2c9de27e7b4916549606400719fff3597f806856
haiyong <sunhy339@126.com>
2016-01-18 21:07:58 +0800

    Initial Commit

---
+++ Tetris/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : Adam Sun
+//  Project    : Tetris
+//  Date       : 1/18/16
+//
+//  Copyright (c) 2016 Adam Sun.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tetris/Classes/HelloWorldScene.h
@@ -0,0 +1,61 @@
+//
+//  HelloWorldScene.h
+//
+//  Created by : Adam Sun
+//  Project    : Tetris
+//  Date       : 1/18/16
+//
+//  Copyright (c) 2016 Adam Sun.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
+- (instancetype)init;
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
